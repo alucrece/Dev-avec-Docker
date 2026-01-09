c6dbd29834d58c474b7b9028319b86d942ae6dbf
alucrece <aliebonglucrece123@gmail.com>
2026-01-09 18:40:02 +0100

    Refactor database connection logic and update student retrieval endpoint

--- postgres-test.py
+++ postgres-test.py
@@ -1,8 +1,9 @@
 import psycopg2
 import os
-import mysql.connector
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from psycopg2.extras import RealDictCursor
+
 app = FastAPI()
 origins = ["*"]
 
@@ -14,26 +15,25 @@ app.add_middleware(
     allow_headers=["*"],
 )
 #connexion
-conn = psycopg2.connect(
-    host="localhost",
-    port=5432,
-    user=os.getenv("POSTGRES_USER", "user_ynov"),
-    password=os.getenv("POSTGRES_PASSWORD"),
-    database=os.getenv("POSTGRES_DB", "mydb")
-)
+def get_db_connection():
+    return psycopg2.connect(
+        host=os.getenv("POSTGRES_HOST", "db"),
+        port=5432,
+        user=os.getenv("POSTGRES_USER", "user_ynov"),
+        password=os.getenv("POSTGRES_PASSWORD"),
+        database=os.getenv("POSTGRES_DB", "mydb")
+    )
 
 
 @app.get("/")
-async def get_version():
-    cur = conn.cursor()
-    cur.execute("SELECT VERSION();")
-    row = cur.fetchone()
-    print(row[0])
-    return {"version": row[0]}
-
-# Test
-cur = conn.cursor()
-cur.execute("SELECT * from users;")
-print(cur.fetchall())
-
-conn.close()
+async def get_students():
+    try:
+        conn = get_db_connection()
+        cur = conn.cursor(cursor_factory=RealDictCursor)
+        cur.execute("SELECT nom, promo FROM students;")
+        row = cur.fetchall()
+        cur.close()
+        conn.close()
+        return row
+    except Exception as e:
+        return {"error": str(e)}
